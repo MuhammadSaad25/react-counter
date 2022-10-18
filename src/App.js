2a061e712aa1bd8f799214f3af853ae5b45ace9b
Muhammad Saad <muhammadsaad2522@gmail.com>
2022-10-18 15:03:04 -0700

    incomplete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import Counter from "./components/main";
+import NavBar from "./components/nav";
 
 function App() {
   return (
     <div className="App">
+      {/* <NavBar/> */}
       <Counter />
     </div>
   );
